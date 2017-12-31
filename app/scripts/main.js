110d0deac01c65f1616924faf3c510c9971f39db
Stephen Lindberg <stwelin@gmail.com>
2017-12-31 17:28:20 -0500

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,113 @@
+var THREE = require('three');
+var dat = require('./vendor/dat.gui.js');
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize( 400,400 );
+renderer.setClearColor( 0xffffff, 0 );
+document.getElementById("colorContainer").appendChild( renderer.domElement );
+
+var colorUniforms = {
+	resolution: { value: new THREE.Vector2() },
+
+	segments: { value: 12 },
+
+	topLeft: {value: new THREE.Vector3(0,0,0) },
+	topRight: {value: new THREE.Vector3(0,0,0) },
+	bottomLeft: {value: new THREE.Vector3(0,0,0) },
+	bottomRight: {value: new THREE.Vector3(0,0,0) }
+}
+
+var shaderMaterial = new THREE.ShaderMaterial( {
+
+	uniforms: colorUniforms,
+	vertexShader: document.getElementById( 'vertexProgram' ).textContent,
+	fragmentShader: document.getElementById( 'fragmentProgram' ).textContent
+
+} );
+
+var cube;
+//var loader = new THREE.JSONLoader();
+// loader.load( 'assets/test.json', function(geom){
+// 	cube = new THREE.Mesh( geom, shaderMaterial );
+// 	scene.add( cube );
+// 	animate();
+// });
+
+var geom = new THREE.PlaneGeometry(1,1);
+cube = new THREE.Mesh( geom, shaderMaterial );
+scene.add( cube );
+
+camera.position.z = 1;
+
+renderer.render( scene, camera );
+
+animate();
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	renderer.render( scene, camera );
+}
+
+function colorVector3(color) {
+	let c = new THREE.Color(color);
+	return new THREE.Vector3(c.r, c.g, c.b);
+}
+
+function updateColorUniform() {
+	colorUniforms.topLeft.value = colorVector3(colors.topLeft);
+	colorUniforms.topRight.value = colorVector3(colors.topRight);
+	colorUniforms.bottomLeft.value = colorVector3(colors.bottomLeft);
+	colorUniforms.bottomRight.value = colorVector3(colors.bottomRight);
+}
+
+function changeHue(color, value) {
+	let c = colors.topLeft.getHSL();
+	colors.topLeft.setHSL(value, params.Saturation, params.Lightness);
+}
+
+function changeSat(color, value) {
+	let c = colors.topLeft.getHSL();
+	colors.topLeft.setHSL(params.Hue, value, params.Lightness);
+}
+
+function changeValue(color, value) {
+	let c = colors.topLeft.getHSL();
+	colors.topLeft.setHSL(params.Hue, params.Saturation, value);
+}
+
+
+var colors = {
+	topLeft: "#4ae8e8",
+	topRight: "#7f17f5",
+	bottomLeft: "#b2f219",
+	bottomRight: "#e83131"
+};
+
+updateColorUniform();
+
+var params = {
+	segments: 7
+}
+
+var gui = new dat.GUI({autoPlace: false});
+window.gui = gui;
+var folder = gui.addFolder( 'Colors' );
+folder.addColor( colors, 'topLeft').onChange(updateColorUniform);
+folder.addColor( colors, 'topRight').onChange(updateColorUniform);
+folder.addColor( colors, 'bottomLeft').onChange(updateColorUniform);
+folder.addColor( colors, 'bottomRight').onChange(updateColorUniform);
+var folder = gui.addFolder( 'Parameters' );
+folder.add(params, 'segments' , 3, 9).step(1).onChange(function (value) {
+	shaderMaterial.uniforms.segments.value = value;
+})
+folder.open();
+
+document.getElementById('datGui').appendChild(gui.domElement);
+
+// window.onload = function() {
+//
+// };
---
+++ gulpfile.js
@@ -0,0 +1,195 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', ['build'], function () {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    })
+});
+
+//SCRIPTS (Vendor JS)
+const vendor_externals = ['three']
+
+var vendors = {
+    b: browserify({
+        debug: true
+    })
+    .require(vendor_externals),
+    build: function () {
+        return vendors.b.bundle()
+            // log errors if they happen
+            .on('error', $.util.log.bind($.util, 'Browserify Error'))
+            .pipe(source('vendor.js'))
+            // optional, remove if you don't need to buffer file contents
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+            // Add transformation tasks to the pipeline here.
+            //.pipe($.uglify())
+            .pipe(sourcemaps.write('./')) // writes .map file
+            .pipe(gulp.dest('./dist/scripts/'))
+    },
+    reload: function () {
+        return vendors.build()
+            .pipe(browserSync.stream());
+    },
+    watch: function () {
+        watchify(vendors.b)
+            .on('update', vendors.reload)
+            .on('time', function (time) {
+                $.util.log($.util.colors.green('[✔] Finished Vendors in:'), $.util.colors.cyan(time + 'ms'));
+            });
+        return vendors.reload();
+    }
+}
+
+gulp.task('build-vendors', ['clean'], vendors.build);
+gulp.task('watch-vendors', ['browser-sync'], vendors.watch);
+gulp.task('reload-vendors', vendors.reload);
+
+//APPLICATION (JS)
+var scripts = {
+    b: browserify({
+        entries: ['./app/scripts/main.js'],
+        debug: true
+    })
+    .external(vendor_externals),
+    build: function () {
+        return scripts.b.bundle()
+            .pipe(source('main.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+            // Add transformation tasks to the pipeline here.
+            //.pipe($.uglify())
+            .pipe(sourcemaps.write('./')) // writes .map file
+            .pipe(gulp.dest('./dist/scripts/'))
+    },
+    reload: function () {
+        return scripts.build()
+            .pipe(browserSync.stream());
+    },
+    watch: function () {
+        return gulp.watch('app/scripts/main.js', ['reload-scripts']);
+    }
+}
+
+gulp.task('build-scripts', ['clean'], scripts.build);
+gulp.task('watch-scripts', ['browser-sync'], scripts.watch);
+gulp.task('reload-scripts', scripts.reload);
+
+
+//PAGES (HTML)
+var pages ={
+    build: function () {
+        return gulp.src('app/**/*.html')
+            // Output files
+            .pipe($.if('*.html', $.size({title: 'html', showFiles: true})))
+            .pipe(gulp.dest('dist'))
+    },
+    reload: function () {
+        return pages.build()
+            .pipe(browserSync.stream());
+    },
+    watch: function () {
+        return gulp.watch('app/**/*.html', ['reload-pages']);
+    },
+}
+
+gulp.task('build-pages', ['clean'], pages.build);
+gulp.task('watch-pages', ['browser-sync'], pages.watch);
+gulp.task('reload-pages', pages.reload);
+
+
+// STYLES
+var styles ={
+    build: function () {
+        return gulp.src([
+            'app/styles/**/*.scss',
+            'app/styles/**/*.css'
+            ])
+            .pipe($.sourcemaps.init())
+            .pipe($.sass({
+            precision: 10
+            }).on('error', $.sass.logError))
+            .pipe($.if('*.css', $.cleanCss()))
+            .pipe($.size({title: 'styles'}))
+            .pipe($.sourcemaps.write('./'))
+            .pipe(gulp.dest('dist/styles'))
+    },
+    reload: function () {
+        return styles.build()
+            .pipe(browserSync.stream());
+    },
+    watch: function () {
+        return gulp.watch(['app/**/*.css', 'app/**/*.scss'], ['reload-styles']);
+    },
+}
+
+gulp.task('build-styles', ['clean'], styles.build);
+gulp.task('watch-styles', ['browser-sync'], styles.watch);
+gulp.task('reload-styles', styles.reload);
+
+
+//ASSETS (JSON, IMAGES, ETC)
+var assets = {
+  build: function () {
+    $.util.log($.util.colors.yellow('[❗] Building Assets...'));
+    return gulp.src('./app/assets/**/*')
+      .pipe(gulp.dest('./dist/assets'))
+      .on('end', function () {
+        $.util.log($.util.colors.green('[✔] Finished Assets'));
+      });
+  },
+  reload: function () {
+    return assets.build()
+      .pipe(browserSync.stream())
+  },
+  watch: function () {
+    return gulp.watch('app/assets/**/*', ['reload-assets']);
+    //   .on('change', function (event) {
+    //     if(event.type === 'deleted') {
+    //       del(path.relative('./', event.path).replace('app/assets/','dist/assets/'));
+    //       assets.reload();
+    //     }
+    //   })
+    //   ;
+  }
+};
+
+gulp.task('build-assets', ['clean'], assets.build);
+gulp.task('watch-assets', ['browser-sync'], assets.watch);
+gulp.task('reload-assets', assets.reload);
+
+//deletes contents of dist
+gulp.task('clean', () =>
+    del(['.tmp', 'dist/*', '!dist/.git'], {dot: true})
+);
+
+gulp.task('build', [
+  'build-pages',
+  'build-styles',
+  'build-vendors',
+  'build-scripts',
+  'build-assets'
+]);
+
+gulp.task('watch', [
+  'watch-pages',
+  'watch-styles',
+  'watch-vendors',
+  'watch-scripts',
+  'watch-assets'
+]);
+
+gulp.task('default', () => {
+
+});
