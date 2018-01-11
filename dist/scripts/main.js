f223336fab13e5328d785a63b5da8f547148732c
Stephen Lindberg <stwelin@gmail.com>
2018-01-11 13:14:04 -0500

    fix hex conversion

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -38,9 +38,9 @@ function CreatePicker(element, color, vector) {
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? {
-        r: parseInt(result[1], 16)/256,
-        g: parseInt(result[2], 16)/256,
-        b: parseInt(result[3], 16)/256
+        r: parseInt(result[1], 16)/255,
+        g: parseInt(result[2], 16)/255,
+        b: parseInt(result[3], 16)/255
     } : null;
 }
 
